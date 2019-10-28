1c26c8ca32ab88c6cc9520828e998b1f6295fce0
Muhammad Irfan <irfan.abrazak@gmail.com>
2019-10-28 16:47:00 +0800

    File Upload

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 var config = require('./config');
 
 const url = config.mongoUrl;
@@ -53,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
