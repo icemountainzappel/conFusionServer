bddfff13b28eb808662071142f399cb5b37b1628
Muhammad Irfan <irfan.abrazak@gmail.com>
2019-10-28 17:35:25 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '397246601215559',
+    clientSecret: 'c6b009393041ffb1095ef2275497e466'
+  }
 }
